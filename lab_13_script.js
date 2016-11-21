e21dc7ab470d3ba6b7bcc47ed7d14eb1a3851e3f
Christina Bergstrom <bergstromcbb@gmail.com>
2016-11-21 11:57:42 -0500

    upload of all lab 13 files

---
+++ lab_13_script.js
@@ -0,0 +1,72 @@
+	var app = angular.module("sixYearOld", []); 
+
+	app.controller('sixyearController', function($scope){
+
+		$scope.words = ['Detroit', 'Chicago', 'Paris', 'London', 'Athens', 'Atlanta', 'Berlin', 'Miami', 'Cairo', 'Tokyo'];
+		$scope.wordsDec = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+		$scope.moreWords = ['Belle Isle', 'Navy Pier', 'Eiffel Tower', 'London Bridge', 'The Parthenon', 'Great Pyramids', 'London Eye', 'South Beach', 'Berlin Wall', 'Tokyo Imperial Palace'];
+		// $scope.moreWordsDec = ['six', 'seven', 'eight', 'nine', 'ten'];
+		$scope.newWords = [];
+
+
+		$scope.addWords=function() {
+			var word={};
+			word.newWords=$scope.words[Math.floor(Math.random() * $scope.words.length)];
+			// word.deco="";
+			$scope.newWords.push(word);
+			console.log(word);
+
+		};
+
+		$scope.addWordsDec=function() {
+			var word={};
+			word.newWords=$scope.words[Math.floor(Math.random() * $scope.wordsDec.length)];
+			word.deco=$scope.wordsDec[Math.floor(Math.random() * $scope.wordsDec.length)];
+			$scope.newWords.push(word);
+			console.log(word);
+
+		};
+
+		$scope.addMoreWords=function() {
+			var word={};
+			word.newWords=$scope.moreWords[Math.floor(Math.random() * $scope.moreWords.length)];
+			// word.deco="";
+			$scope.newWords.push(word);
+			console.log(word);
+
+		};
+
+		$scope.addMoreWordsDec=function() {
+			var word={};
+			word.newWords=$scope.moreWords[Math.floor(Math.random() * $scope.wordsDec.length)];
+			word.deco=$scope.wordsDec[Math.floor(Math.random() * $scope.wordsDec.length)];
+			$scope.newWords.push(word);	
+			console.log(word);
+
+		};
+
+
+	});
+
+
+// var randNum, finalWord;
+//     randNum = Math.floor(Math.random() * gameWordArray.length);
+//     finalWord = gameWordArray[randNum];
+//     return finalWord;
+
+
+// var getRandomWord = function () {
+//     return words[Math.floor(Math.random() * words.length)];
+// };
+//   $scope. = function(){
+// var item={};
+//  show = words[Math.floor(Math.random() * words.length)];
+//   	 word.something = $scope.chores.push(pickWord);	
+//   	$scope.pickWord = "";
+//   };
+
+  // $scope.removeChore = function(chore){
+  // 	var index = $scope.chores.indexOf(chore);	
+  // 	$scope.chores.splice(index, 1);
+
+  // };
